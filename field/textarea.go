32c7fdf4bdf880d86030ce5453472694ba05ecb7
8lall0 <savino.liguori@gmail.com>
2022-10-31 13:49:05 +0100

    Add missing attributes to textarea template and code.

--- field/textarea.go
+++ field/textarea.go
@@ -3,14 +3,19 @@ package field
 import "io"
 
 type TextArea struct {
-	Id          string
-	Name        string
-	Placeholder string
-	Value       string
-	Label       string
-	Rows        int
-	Cols        int
-	Required    bool
+	Id           string
+	Name         string
+	Placeholder  string
+	Value        string
+	Label        string
+	Rows         int
+	Cols         int
+	MaxLength    int
+	Required     bool
+	ReadOnly     bool
+	AutoFocus    bool
+	AutoComplete bool
+	Disabled     bool
 }
 
 func (t *TextArea) Render(tplPath string, w io.Writer) error {
